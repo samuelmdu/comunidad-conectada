7edfae5f78f2f4bf977d3aa31e83a54bca789d69
DanSword36 <danielguzman2105@gmail.com>
2025-08-04 17:35:22 -0600

    Menú Responsive

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Configuration
 app.set('views', path.join(__dirname, 'views'))
